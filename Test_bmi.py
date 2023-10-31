b9eedb3575325055f159096cf24a79a110f924a0
Han Ni <hanniha16.22@ichat.sp.edu.sg>
2023-10-31 10:13:17 +0800

    Unit Testing for BMI file from Lab 2

---
+++ Test_bmi.py
@@ -0,0 +1,15 @@
+import Lab_2.bmi as bmi
+
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.55,50)
+    assert (result == 1)
+
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.55,70)
+    assert (result == 0)
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.55,30)
+    assert (result == -1)
