87462d65b35f3b698f3c7b699bcac580368da06a
Han Ni <hanniha16.22@ichat.sp.edu.sg>
2023-10-31 11:31:46 +0800

    Unit Testing for lab 3

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -12,6 +12,7 @@ def test_bubble_sort_ascending():
 
     assert (result == test_arr)
 
+
 def test_bubble_sort_descending():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
@@ -20,6 +21,34 @@ def test_bubble_sort_descending():
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
     assert (result == test_arr)
+
+
+def test_bubble_sort_10():
+    result = []
+    input_arr = [2, 4, 6, 8, 10, 1, 3, 5, 7, 9]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 1)
+
+
+def test_bubble_sort_0():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 0)
+
+
+def test_bubble_sort_string():
+    result = []
+    input_arr = ['a', 'b', 'c']
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 2)
+
 
 def test_bubble_sort_invalid():
     result = []
